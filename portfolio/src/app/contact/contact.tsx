581fd43b271dcda09077fdb33230feb076acca71
Lisandro85 <lisandrobedotti@hotmail.com>
2024-12-18 00:14:05 +0100

    fixed styles

--- portfolio/src/app/contact/contact.tsx
+++ portfolio/src/app/contact/contact.tsx
@@ -4,12 +4,12 @@ export default function Contact() {
   return (
     <div className="mx-5 bg-[rgba(0,109,119,0.8)]  rounded-lg shadow-md mt-10 py-10 px-4 mb-96">
       {/* Título */}
-      <h1 className="text-4xl md:text-5xl font-bold text-[#06d6a0] font-Exo-2 text-center">
+      <h1 className="text-6xl md:text-5xl font-bold text-[#06d6a0]  font-Amatic-SC  text-center">
         Contact Me
       </h1>
 
       {/* Descripción */}
-      <p className="text-lg md:text-2xl mt-4 text-[#06d6a0] font-Exo-2 text-center leading-relaxed">
+      <p className="text-lg md:text-4xl mt-4 text-[#06d6a0] font-bold font-Amatic-SC  text-center leading-relaxed">
         Feel free to reach out to me via LinkedIn, GitHub, or email. I&apos;m
         always open to new opportunities and collaborations!
       </p>
@@ -20,7 +20,7 @@ export default function Contact() {
           href="https://www.linkedin.com/in/lisandro-bedotti"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-[#52b69a] text-4xl hover:text-blue-600 transform transition-all duration-300 hover:scale-125"
+          className="text-[#52efc2] text-4xl hover:text-blue-600 transform transition-all duration-300 hover:scale-125"
         >
           <Linkedin />
         </a>
@@ -28,13 +28,13 @@ export default function Contact() {
           href="https://github.com/Lisandro85"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-[#52b69a] text-4xl hover:text-black transform transition-all duration-300 hover:scale-125"
+          className="text-[#52efc2] text-4xl hover:text-black transform transition-all duration-300 hover:scale-125"
         >
           <Github />
         </a>
         <a
           href="mailto:lisandrobedotti@hotmail.com"
-          className="text-[#52b69a] text-4xl hover:text-red-600 transform transition-all duration-300 hover:scale-125"
+          className="text-[#52efc2] text-4xl hover:text-red-600 transform transition-all duration-300 hover:scale-125"
         >
           <Mail />
         </a>
--- portfolio/src/app/projects/projects.tsx
+++ portfolio/src/app/projects/projects.tsx
@@ -5,7 +5,7 @@ export default function Projects() {
         {/* Contenedor de tarjetas con grid responsivo */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
           {/* Tarjeta 4 */}
-          <div className="w-full max-w-sm overflow-hidden bg-[rgba(0,109,119,0.8)]  rounded-lg shadow-md p-4 transition-all duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl">
+          <div className="w-full max-w-sm overflow-hidden bg-[rgba(0,109,119,0.8)] rounded-lg shadow-md p-4 transition-all duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl">
             <a
               href="https://www.youtube.com/watch?v=igOPSYdE_Xc"
               target="_blank"
@@ -18,21 +18,21 @@ export default function Projects() {
               />
             </a>
             <div className="p-4">
-              <h3 className="text-xl font-semibold text-[#52b69a] mb-2">
+              <h3 className="text-2xl font-bold text-[#3ae8d4] mb-2 font-Amatic-SC">
                 Buddify - Social Network
               </h3>
-              <p className="text-sm text-[#83c5be] mb-4">
-                <span className="font-bold text-[#2a9d8f]">Technologies:</span>{" "}
+              <p className="text-xl text-[#52efc2] mb-4 font-Amatic-SC font-bold">
+                <span className="font-bold text-[#3ae8d4]">Technologies:</span>{" "}
                 NestJS, TypeORM, TypeScript, PostgreSQL, Next.js, Tailwind CSS.{" "}
                 <br />
-                <span className="font-bold text-[#2a9d8f]">Features:</span>{" "}
+                <span className="font-bold text-[#3ae8d4]">Features:</span>{" "}
                 Create and join events, user profiles, and activity exploration.
               </p>
               <a
                 href="https://github.com/Lisandro85/Buddify"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-lg font-semibold text-[#52b69a] hover:text-[#76c893] transition-colors duration-300"
+                className="text-lg font-bold text-[#3ae8d4] hover:text-[#76c893] transition-colors duration-300 font-Amatic-SC"
               >
                 See Repository
               </a>
@@ -40,7 +40,7 @@ export default function Projects() {
           </div>
 
           {/* Tarjeta 3 */}
-          <div className="w-full max-w-sm overflow-hidden bg-[rgba(0,109,119,0.8)]  rounded-lg shadow-md p-4 transition-all duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl">
+          <div className="w-full max-w-sm overflow-hidden bg-[rgba(0,109,119,0.8)] rounded-lg shadow-md p-4 transition-all duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl">
             <a
               href="https://www.youtube.com/watch?v=IwXr4UKzs64"
               target="_blank"
@@ -53,20 +53,20 @@ export default function Projects() {
               />
             </a>
             <div className="p-4">
-              <h3 className="text-xl font-semibold text-[#52b69a] mb-2">
+              <h3 className="text-2xl font-bold text-[#3ae8d4] mb-2 font-Amatic-SC">
                 Ecommerce API
               </h3>
-              <p className="text-sm text-[#83c5be] mb-4">
-                <span className="font-bold text-[#2a9d8f]">Technologies:</span>{" "}
+              <p className="text-xl text-[#52efc2] mb-4 font-Amatic-SC font-bold">
+                <span className="font-bold text-[#3ae8d4]">Technologies:</span>{" "}
                 NestJS, PostgreSQL, TypeScript, TypeORM. <br />
-                <span className="font-bold text-[#2a9d8f]">Features:</span>{" "}
+                <span className="font-bold text-[#3ae8d4]">Features:</span>{" "}
                 Product, user, and order management.
               </p>
               <a
                 href="https://github.com/Lisandro85/ecommerceDeploy"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-lg font-semibold text-[#52b69a] hover:text-[#76c893] transition-colors duration-300"
+                className="text-lg font-bold text-[#3ae8d4] hover:text-[#76c893] transition-colors duration-300 font-Amatic-SC"
               >
                 See Repository
               </a>
@@ -74,7 +74,7 @@ export default function Projects() {
           </div>
 
           {/* Tarjeta 2 */}
-          <div className="w-full max-w-sm overflow-hidden bg-[rgba(0,109,119,0.8)]  rounded-lg shadow-md p-4 transition-all duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl">
+          <div className="w-full max-w-sm overflow-hidden bg-[rgba(0,109,119,0.8)] rounded-lg shadow-md p-4 transition-all duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl">
             <a
               href="https://www.youtube.com/watch?v=VizIYWYvxLQ"
               target="_blank"
@@ -87,20 +87,20 @@ export default function Projects() {
               />
             </a>
             <div className="p-4">
-              <h3 className="text-xl font-semibold text-[#52b69a] mb-2">
+              <h3 className="text-2xl font-bold text-[#3ae8d4] mb-2 font-Amatic-SC">
                 Rest-Bar Scheduling System
               </h3>
-              <p className="text-sm text-[#83c5be] mb-4">
-                <span className="font-bold text-[#2a9d8f]">Technologies:</span>{" "}
+              <p className="text-xl text-[#52efc2] mb-4 font-Amatic-SC font-bold">
+                <span className="font-bold text-[#3ae8d4]">Technologies:</span>{" "}
                 TypeScript, PostgreSQL, Express, React. <br />
-                <span className="font-bold text-[#2a9d8f]">Features:</span>{" "}
+                <span className="font-bold text-[#3ae8d4]">Features:</span>{" "}
                 Real-time reservations and customer management.
               </p>
               <a
                 href="https://github.com/Lisandro85/sistema-de-turnos-resto-bar"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-lg font-semibold text-[#52b69a] hover:text-[#76c893] transition-colors duration-300"
+                className="text-lg font-bold text-[#3ae8d4] hover:text-[#76c893] transition-colors duration-300 font-Amatic-SC"
               >
                 See Repository
               </a>
@@ -121,21 +121,21 @@ export default function Projects() {
               />
             </a>
             <div className="p-4">
-              <h3 className="text-xl font-semibold text-[#52b69a] mb-2">
+              <h3 className="text-2xl font-bold text-[#3ae8d4] mb-2 font-Amatic-SC">
                 Movie Platform
               </h3>
-              <p className="text-sm text-[#83c5be] mb-4">
-                <span className="font-bold text-[#2a9d8f]">Technologies:</span>{" "}
+              <p className="text-xl text-[#52efc2] mb-4 font-Amatic-SC font-bold">
+                <span className="font-bold text-[#3ae8d4]">Technologies:</span>{" "}
                 JavaScript, HTML, CSS, Bootstrap, Mongoose, MongoDB, Express.{" "}
                 <br />
-                <span className="font-bold text-[#2a9d8f]">Features:</span>{" "}
+                <span className="font-bold text-[#3ae8d4]">Features:</span>{" "}
                 Movie catalog and user management.
               </p>
               <a
                 href="https://github.com/Lisandro85/Movies-Plataforma"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-lg font-semibold text-[#52b69a] hover:text-[#76c893] transition-colors duration-300"
+                className="text-lg font-bold text-[#3ae8d4] hover:text-[#76c893] transition-colors duration-300 font-Amatic-SC"
               >
                 See Repository
               </a>

bce050c6a397e3091ea1ab1cc337e07c48bd6cbc
Lisandro85 <lisandrobedotti@hotmail.com>
2024-12-17 11:21:40 +0100

    config

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+// next.config.js
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+module.exports = nextConfig;

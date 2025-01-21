e3b7406e095d29de8654e658302e1db6e8469672
Lisandro85 <lisandrobedotti@hotmail.com>
2025-01-21 09:50:48 +0100

    new contactme

---
+++ portfolio/src/app/contact/contactMethod.tsx
@@ -0,0 +1,42 @@
+import { ReactNode } from "react";
+
+type ContactMethodProps = {
+  icon: ReactNode;
+  text: string;
+  link: string;
+};
+
+export default function ContactMethod({
+  icon,
+  text,
+  link,
+}: ContactMethodProps) {
+  return (
+    <>
+      <p className="text-emerald-500">
+        {" "}
+        <span className="text-gray-400 ml-10 sm:ml-14 md:ml-16 lg:ml-20">
+          {"<"}
+        </span>
+        ContactMethod
+      </p>
+      <div className="flex items-center space-x-4">
+        <p className="text-blue-400 ml-12 sm:ml-16 md:ml-20 lg:ml-24">icon:</p>
+        <a
+          href={link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-[#52efc2] text-5xl sm:text-6xl md:text-7xl lg:text-8xl hover:text-blue-600 transform transition-all duration-300 hover:scale-125"
+        >
+          {icon}
+        </a>
+      </div>
+      <p className="text-blue-400 ml-12 sm:ml-16 md:ml-20 lg:ml-24">
+        text: <span className="text-amber-800">{text}</span>
+      </p>
+      <p className="text-blue-400 ml-12 sm:ml-16 md:ml-20 lg:ml-24">
+        link: <span className="text-amber-800">{link}</span>
+      </p>
+    </>
+  );
+}
---
+++ portfolio/src/app/contact/contactWithCode.tsx
@@ -0,0 +1,43 @@
+import { GithubIcon, Linkedin, Mail } from "lucide-react";
+import ContactMethod from "./contactMethod";
+
+export default function contactWithCode() {
+  return (
+    <div className="flex items-center justify-center w-full h-screen px-4 sm:px-6 lg:px-10 mt-10">
+      <div className="w-full max-w-xl bg-[rgba(0,0,0,0.8)] p-5 rounded-lg shadow-xl">
+        <p>
+          <span className="text-violet-500">if {""}</span>
+          <span className="text-blue-900">
+            {"(youCanContactme)"} {""}
+            <span className="text-violet-500">{"{"}</span>
+          </span>
+        </p>
+        <p className="text-violet-500 ml-5 sm:ml-10 md:ml-12 lg:ml-16">
+          {"return ("}
+        </p>
+        <p className="text-gray-400 ml-10 sm:ml-12 md:ml-14 lg:ml-16">{"<>"}</p>
+        <ContactMethod
+          icon={<Linkedin />}
+          text="Linkedin"
+          link="https://www.linkedin.com/in/lisandro-bedotti"
+        />
+        <ContactMethod
+          icon={<GithubIcon />}
+          text="Github"
+          link="https://github.com/Lisandro85"
+        />
+        <ContactMethod
+          icon={<Mail />}
+          text="Mail"
+          link="mailto:lisandrobedotti@hotmail.com"
+        />
+        <p className="text-gray-400 ml-12 sm:ml-14 md:ml-16 lg:ml-20">{"/>"}</p>
+        <p className="text-gray-400 ml-10 sm:ml-12 md:ml-14 lg:ml-16">
+          {"</>"}
+        </p>
+        <p className="text-violet-500 ml-8 sm:ml-10 md:ml-12 lg:ml-14">{")"}</p>
+        <p className="text-violet-500 ml-5 sm:ml-8 md:ml-10 lg:ml-8">{"}"}</p>
+      </div>
+    </div>
+  );
+}
